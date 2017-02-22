e980095dbf0a6c59b688ae64d5869bc5d3595470
Victor <vkingmaker1@gmail.com>
2017-02-22 18:50:51 +0100

    Updated ComicBook Detail View to be strongly Typed

--- ComicBookDiary/Controllers/ComicBookController.cs
+++ ComicBookDiary/Controllers/ComicBookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookDiary.Models;
 
 namespace ComicBookDiary.Controllers
 {
@@ -10,10 +11,23 @@ namespace ComicBookDiary.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.Description = "The Lord is Always Good Even in the days of trouble";
-            ViewBag.Price = 700;
+            ComicBook comicBook = new ComicBook
+            {
+                SeriesTitle = "The Amazing Spider-man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final Issue! Witness the final hours of Doctor Octopus' Life and his one,Last,great act of revenge! Even if Spider Man Survives...<strong>Will Peter Parker</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name="Dan Sloot" , Role="Script"},
+                    new Artist() { Name="Humberto Ramos" , Role="Pencils"},
+                    new Artist() { Name="Victor Olazaba" , Role="Inks"},
+                    new Artist() { Name="Elgar Delgado" , Role="Colors"},
+                    new Artist() { Name="Letters" , Role="Monday Victor"}
+                },
 
-            return View();
+            };
+
+            return View(comicBook);
         }
     }
 }
--- ComicBookDiary/Models/ComicBook.cs
+++ ComicBookDiary/Models/ComicBook.cs
@@ -10,8 +10,8 @@ namespace ComicBookDiary.Models
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string Description { get; set; }
-        public Artist[] Artist { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
         public string DisplayText
